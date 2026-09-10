54f7a3f3d0d9cc14a23c2c7b365fc47a48b740b1
Anna Karutina <anna.karutina@khk.ee>
2026-09-10 11:14:37 +0300

    Week 10 - Exercise 31.2

--- src/week10/Main.java
+++ src/week10/Main.java
@@ -11,5 +11,27 @@ public class Main {
         tank = new BulkTank(50);
         tank.addToTank(100);
         System.out.println(tank);
+
+        Cow cow = new Cow();
+        System.out.println(cow);
+
+        Alive livingCow = cow;
+        livingCow.liveHour();
+        livingCow.liveHour();
+        livingCow.liveHour();
+        livingCow.liveHour();
+        System.out.println(cow);
+        Milkable milkingCow = cow;
+        milkingCow.milk();
+        System.out.println(cow);
+        System.out.println("");
+
+        cow = new Cow("Ammu");
+        System.out.println(cow);
+        cow.liveHour();
+        cow.liveHour();
+        System.out.println(cow);
+        cow.milk();
+        System.out.println(cow);
     }
 }
